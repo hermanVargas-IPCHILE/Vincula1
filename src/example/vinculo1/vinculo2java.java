27a8ea542725926326b680d21143d204c20b790c
Herman <herman@consult-tic.cl>
2014-06-11 21:58:36 -0400

    Mi primer Commit

---
+++ src/example/vinculo1/vinculo2java.java
@@ -0,0 +1,21 @@
+package example.vinculo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class vinculo2java extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.vinculo2);
+	}
+	public void salir(View view){
+		finish();
+	}
+
+	
+	
+}
