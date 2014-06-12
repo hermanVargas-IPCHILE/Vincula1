6ec51ac440e0672c7959bbca566810c9934e1787
Herman <herman@consult-tic.cl>
2014-06-11 22:15:25 -0400

    este es el cuarto commit

--- src/example/vinculo1/vinculo2java.java
+++ src/example/vinculo1/vinculo2java.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class vinculo2java extends Activity {
-
+//hola este es otro comentario
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
